c3dcfea59b8c0b72317461ab5f089a443ffb21ff
samdenty99 <sam@samdd.me>
2017-08-10 21:02:20 +0100

    Added files

--- README.md
+++ README.md
@@ -1,1 +1,129 @@
 # HologramPlusAPI
+
+**What does this project do?**
+
+This lightweight project enhances the [`Hologram.io`](https://hologram.io) Rest [API for sending messages](https://hologram.io/docs/reference/cloud/http/#/reference/hologram-cloud/sms/send-sms-to-a-device) by adding useful features & variables. These include:
+
+- Automatically send long SMS as multiple 160 character messages -  see [overflow](#overflow)
+- SMS variables (handled server side) - see [variables](#variables)
+- Easy SMS formatting - see [formatting](#formatting) 
+
+NOTE: You can use the demo server over at [`http(s)://s.samdd.me/hologram`](https://s.samdd.me/hologram) if you don't want to host your own instance.
+
+Here are some examples of what you can do:
+### Basic hello world
+    hologram.php?key=KEY&id=device
+> [12:30] Hello world! (No message specified)
+
+### Variables
+    hologram.php?key=KEY&id=device&body=IP={$IP$}
+> IP=34.168.56.2
+
+<br>
+
+    hologram.php?key=KEY&id=device&body=The time is {$time$}
+> The time is 12:30
+
+<br>
+
+    hologram.php?key=KEY&id=device&body=Hello{$nl$}World
+> Hello
+> World
+
+<br>
+
+    hologram.php?key=KEY&id=device&body={$pre$}Example title{$nl2$}Lorem ipsum
+> [12:30]: Example title
+> 
+> Lorem ipsum
+
+### Formatting
+
+    hologram.php?key=KEY&id=device&title=Test&body=Hello world
+> [12:30]: Test: Hello world
+
+<br>
+
+    hologram.php?key=KEY&id=device&title=Notification{$nl$}&body=Lorem ipsum
+
+> \[12:30\]: Notification:
+> 
+> Lorem ipsum
+
+## SMS configuration
+### Overflow
+By default messages are split into multiple SMS, though this can easily be change by using the `?overflow` parameter. 
+
+    overflow=multiple     (default) Messages will be split into multiple SMSs
+    overflow=trim         Messages will be trimmed so that they are 160 characters long
+    overflow=fail         Messages will not be sent if they exceed 160 characters
+
+### Timezone
+By default `HologramPlusAPI` uses the `Europe/London` timezone, but this can easily be changed by specifying the `?timezone` parameter. Refer to the [timezone lists here](http://php.net/manual/en/timezones.php)
+
+    timezone=America/New_York     Change the {$time$} variables to the New York timezones.
+
+### Footer
+Normally only one message is sent (aka the title+body). You can specify a message to be sent after all the other messages are sent by using the `?footer` parameter. Example:
+
+    footer=//END OF MESSAGE      Send a separate SMS after the main one(s) 
+
+### Sandbox
+If you want to test out your messages without actually sending them, add the `&sandbox` parameter to the URL.
+
+## Example URLs
+    https://s.samdd.me/hologram?key=KEY&id=DEVICE
+    https://s.samdd.me/hologram?key=KEY&id=DEVICE&title=Hello{$nl$}&body=World
+	https://s.samdd.me/hologram?key=KEY&id=DEVICE&overflow=trim&body=REALLY LONG MESSAGE...
+	https://s.samdd.me/hologram?key=KEY&id=DEVICE&timezone=America/New_York&body={$time2$}
+	https://s.samdd.me/hologram?key=KEY&id=DEVICE&body=Sender IP={$ip$}
+
+## Full Usage documentation:
+	USAGE:
+
+	REQUIRED:
+		?key  = Hologram API Key
+		?id   = Hologram device ID 
+
+	OPTIONAL:
+		?body 		= The message to be sent to the device (Messages will be split up if over 160 characters, unless overflow parameter set)
+						Default:	[TIME] Message delivered
+
+		?from 		= The number to send the message from
+						Default = 7367125
+								  TESTING
+
+		?timezone	= The PHP timezone to use
+						Default: 	Europe/London
+
+		?overflow	= How to handle messages exceeding 160 characters
+							multiple	| split messages into multiple SMSs
+							trim		| trim end of messages
+							fail		| don't send message if it exceeds 160 characters
+						Default: multiple
+
+	    ?title		= Prefix message with a title, format=[H:M] TITLE:
+	    				EXAMPLE: '[12:30] Example title: '
+	    				Default: none
+
+	    ?footer		= Send a separate SMS after the content SMS's (eg. for a link / message separator SMS etc.)
+
+	    ?sandbox	= Prevents the message from being sent, (use for API testing purposes)
+
+	BODY VARIABLES (case sensitive):
+		{$time$}	The current time, format=H:M
+					EXAMPLE: '12:30'
+
+		{$time2$}	The current time, format=H:M:S
+						EXAMPLE: '12:30:05'
+
+		{$pre$}		Message time prefix, format=[H:M:S]:
+						EXAMPLE: '[12:30]: '
+
+		{$ip$}		IP Address 
+
+		{$nl$} 		Insert newline
+
+		{$nl2$} 	Insert two newlines
+
+		{$nl3$} 	Insert three newlines
---
+++ hologram.php
@@ -0,0 +1,142 @@
+<?php
+header('Content-Type: application/json');
+
+if (isset($_GET['key']) && isset($_GET['id'])) {
+	$key = $_GET['key'];
+	$id = $_GET['id'];
+
+	if (isset($_GET['timezone']))
+		date_default_timezone_set($_GET['timezone']);
+	else
+		date_default_timezone_set("Europe/London"); // Default Timezone
+
+	// Extract the From Number from URL, if it doesn't exist use default
+	if (isset($_GET['from']))
+		$from = $_GET['from'];
+	else
+		$from = "7367125"; // Default from number
+
+	// Extract the overflow value from URL, if it doesn't exist use default
+	if (isset($_GET['overflow']))
+		$overflow = $_GET['overflow'];
+	else
+		$overflow = "multiple"; // Default message split type
+
+	// Extract the Body message from URL, if it doesn't exist use default
+	if (isset($_GET['body'])){
+		// Prefix the body message with Title value (if it's been specified)
+		if (isset($_GET['title'])) {
+			if (substr($_GET['title'], -6) == "{\$nl\$}")
+				$body = "[" . date("H:i") . "] " . substr($_GET['title'], 0, -6) . ":{\$nl\$}" . $_GET['body'];
+			else
+				$body = "[" . date("H:i") . "] " . $_GET['title'] . ": " . $_GET['body'];
+		}
+		// Evaluate body variables
+			$find 	 = array( "{\$time\$}"	, "{\$time2\$}"	, "{\$pre\$}"				, "{\$ip\$}"			 , "{\$nl\$}" , "{\$nl2\$}" , "{\$nl3\$}");
+			$replace = array( date("H:i")	, date("H:i:s")	, "[" . date("H:i") . "]: "	, $_SERVER['REMOTE_ADDR'], "\\n"		  , "\\n\\n"		, "\n\n\n");
+			$body 	 = str_replace($find, $replace, $body);
+
+		// If the message is more the 160 characters (Max SMS length)
+		if (strlen($body) > 160) {
+			// TRIM: Trim off the excess
+			if ($overflow == "trim") {
+				$body = substr($body,0,160);
+			} else if ($overflow == "fail") {
+				// FAIL: Don't send the message, show an error and exit
+				$responce = array("success" => false, "error" => 2, "info" => "Message exceeded 160 characters!");
+				echo json_encode($responce);
+				exit();
+			} else {
+				// Create an Array each value containing 160 characters of the message
+				$body_array = str_split($body, 160);
+			}
+		}
+	} else {
+		$body = "[" . date("H:i") . "] Hello world! (No message specified)"; // Default body message
+	}
+
+	// Skip if the sandbox parameter specified
+	if (!isset($_GET['sandbox'])) {
+		// If the body is an array (ie. It needs to be sent in multiple messages)
+		if ($body_array) {
+			foreach($body_array as $sms) {
+				$data = array("deviceid" => $id, "fromnumber" => $from, "body" => $sms);
+				$data_string = json_encode($data);
+				$data_string = str_replace("\\n", 'n', $data_string);
+				$ch = curl_init('https://dashboard.hologram.io/api/1/sms/incoming?apikey=' . $key);
+				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");   
+				curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);    
+				curl_setopt($ch, CURLOPT_HTTPHEADER, array(        
+				    'Content-Type: application/json',              
+				    'Content-Length: ' . strlen($data_string))     
+				);                                                 
+				$result = curl_exec($ch);
+			}
+			$times = count($body_array);
+		// Else send a single SMS
+		} else {
+			$data = array("deviceid" => $id, "fromnumber" => $from, "body" => $body);
+			$data_string = json_encode($data);
+			$data_string = str_replace("\\n", 'n', $data_string);
+			$ch = curl_init('https://dashboard.hologram.io/api/1/sms/incoming?apikey=' . $key);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");   
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);    
+			curl_setopt($ch, CURLOPT_HTTPHEADER, array(        
+			    'Content-Type: application/json',              
+			    'Content-Length: ' . strlen($data_string))     
+			);                                                 
+			$result = curl_exec($ch);
+			$times = 1;
+		}
+		// If the footer parameter was specified then send another SMS with it's value
+		if (isset($_GET['footer'])) {
+			$data = array("deviceid" => $id, "fromnumber" => $from, "body" => $_GET['footer']);
+			$data_string = json_encode($data);
+			$data_string = str_replace("\\n", 'n', $data_string);
+			$ch = curl_init('https://dashboard.hologram.io/api/1/sms/incoming?apikey=' . $key);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");   
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);    
+			curl_setopt($ch, CURLOPT_HTTPHEADER, array(        
+			    'Content-Type: application/json',              
+			    'Content-Length: ' . strlen($data_string))     
+			);                                                 
+			$result2 = curl_exec($ch);
+		}
+	// Show example output if ?sandbox parameter specified
+	} else {
+		if ($body_array)
+			$times = count($body_array);
+		else
+			$times = 1;
+		$responce = array("success" => true, "error" => 0, "info" => "Message delivered! [" . $times . " sent]", "message" => $body, "testing" => true);
+		$example = json_encode($responce);
+		$example = str_replace("\\\\n", '\\n', $example);
+		echo $example;
+		exit();
+	}
+
+	// If the CURL output is empty
+	if (!$result) {
+		$responce = array("success" => false, "error" => 3, "info" => "Failed to connect to the Hologram API!");
+		echo json_encode($responce);
+		exit();
+	}
+
+	// Decode the CURL output to an array
+	$result_array = json_decode($result, true);
+	if ($result_array[0]["success"] == true) {
+		$responce = array("success" => true, "error" => 0, "info" => "Message delivered! [" . $times . " sent]", "message" => $body);
+	} else {
+		$responce = array("success" => false, "error" => 4, "info" => $result_array["error"]);
+	}
+	echo str_replace("\\n", 'n', json_encode($responce));
+
+// Callback if API key or ID not specified
+} else {
+	$responce = array("success" => false, "error" => 1, "info" => "No API key or device ID specified!");
+	echo json_encode($responce);
+}
+?>
